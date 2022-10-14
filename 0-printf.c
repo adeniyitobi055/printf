15a6a73e5ae0b0d75f3882af22e0e1a7873a2b88
efezino idisi <enduranceidisi13@gmail.com>
2022-10-14 16:08:32 +0100

    add task 0 solution

---
+++ 0-printf.c
@@ -0,0 +1,46 @@
+#include <unistd.h>
+#include <stdarg.h>
+
+/**
+ * _printf - produces output according to format
+ * format: format string
+ *
+ * Return: Always 0
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == 'c')
+			{
+				char a = va_arg(args, int);
+				write(1, &a, 1);
+				i++;
+			}
+			else if (format[i + 1] == 's')
+			{
+				unsigned int j = 0;
+				char *b = va_arg(args, char *);
+				while (b[j] != '\0')
+				{
+					write(1, &b[j], 1);
+					j++;
+				}
+				i++;
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+		}
+	}
+	va_end(args);
+	return (0);
+}
