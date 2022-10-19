f68d44224fa9f9e154b83b4d66ae5d447a817c5b
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-10-18 23:52:34 -0700

    print hexa lowercase

--- conversion_functions.c
+++ conversion_functions.c
@@ -22,3 +22,54 @@ int print_octal(unsigned int num)
 	len++;
 	return (len);
 }
+
+/**
+ * print_hexa - convert a number to hexadecimal(lowercase)
+ * @num: number.
+ *
+ * Return: Nothing.
+ */
+
+int print_hex(unsigned int num)
+{
+	int quotient, remainder, len = 0;
+
+	if (num == 0)
+	{
+		return (len);
+	}
+	quotient = num / 16;
+	remainder = num % 16;
+	len += print_hex(quotient);
+	switch (remainder)
+	{
+		case 10:
+			print_char('a');
+			len++;
+			break;
+		case 11:
+			print_char('b');
+			len++;
+			break;
+		case 12:
+			print_char('c');
+			len++;
+			break;
+		case 13:
+			print_char('d');
+			len++;
+			break;
+		case 14:
+			print_char('e');
+			len++;
+			break;
+		case 15:
+			print_char('f');
+			len++;
+			break;
+		default:
+			print_char(remainder + 48);
+			len++;
+	}
+	return (len);
+}	
